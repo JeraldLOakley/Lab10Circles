1076e4f166ca111916088b2705e7279208e55ab1
JeraldLOakley <Jerryoakley989@gmail.com>
2018-05-04 13:03:03 -0400

    first commit

---
+++ Lab10/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Circle
+    {
+
+
+         double Radius;
+
+        
+
+        public Circle (double Radius)
+        {
+            this.Radius = Radius;    
+        }
+       
+        public double CalculateCircumference()   
+        {    
+            return Math.Round(Radius * 2 * Math.PI,2);
+        }
+
+
+        public double CalculateArea()
+        {
+            return Radius * Radius * Math.PI;
+        }
+
+
+
+       
+       
+        //public string CalculateFormattedCircumference()
+      
+        //public string CalculateFormattedArea()
+        //private string FormatNumber (Number x)
+        //Define a member called radius. This member should be private.
+        //define a property to get access to the class member: Radius
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine();
+            Console.WriteLine("Welcome to circumference circulator");
+            Console.WriteLine();
+            Console.WriteLine("If you would like to find the area and circumference of a circle please type a radius");
+            
+           string input= Console.ReadLine();
+           
+            bool success = double.TryParse(input, out double Radius);
+            if (success)
+            {
+                Circle circle = new Circle(Radius);
+                Console.WriteLine($"The circumference of a circle is {circle.CalculateCircumference()}");
+                Console.WriteLine($"The area of a circle is {circle.CalculateArea()}");
+            }
+            else
+                Console.WriteLine("Not a number");
+            
+
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
